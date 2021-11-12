5b38a346ab3df84ccc7ecbcf10bb255fccaa3c98
wadientk <wadientk@gmail.com>
2021-11-12 14:37:55 +0900

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = '';
+
+  const div = document.createElement('div');
+  div.className = 'list-row';
+
+  const li = document.createElement('li');
+  li.innerText = inputText;
+
+  div.appendChild(li);
+
+  console.log(div, li);
+};
+
+// Events
+document
+  .getElementById('add-button')
+  .addEventListener('click', () => onClickAdd());
